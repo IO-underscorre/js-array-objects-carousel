4b60bd6a7b4cab53a926c6121d3008cccd82e056
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-20 13:44:34 +0200

    feat: buttons added to stop and play the automatic carousel scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,7 @@ const games = [
 ];
 
 const primaryImageContainer = document.getElementById('changing-image');
-const thumbnailContainers = document.getElementsByClassName('thumbnail');
+const thumbnailContainers = Array.from(document.getElementsByClassName('thumbnail'));
 const btns = document.getElementsByTagName('button');
 
 // Insert images in the page
@@ -32,30 +32,43 @@ games.forEach((game , i) => {
     image.src = game.image;
     image.alt = game.title;
 
-    const textAndDescription = document.createElement('div');
-    textAndDescription.innerHTML = `<h2>${game.title}</h2><p>${game.text}</p>`;
+    const titleAndDescription = document.createElement('div');
+    titleAndDescription.innerHTML = `<h2>${game.title}</h2><p>${game.text}</p>`;
+    titleAndDescription.classList.add('title-and-description');
 
     primaryImageContainer.append(image);
-    primaryImageContainer.append(textAndDescription);
+    primaryImageContainer.append(titleAndDescription);
     thumbnailContainers[i].append(image.cloneNode(true));
 });
 
 const primaryImages = Array.from(document.querySelectorAll('#changing-image > img'));
-const gameDescriptions = document.querySelectorAll('#changing-image > div');
+const gameDescriptions = Array.from(document.querySelectorAll('.title-and-description'));
 
 primaryImages[0].classList.add('active');
 thumbnailContainers[0].classList.add('active');
 gameDescriptions[0].classList.add('active');
 
+// Auto-scroll call
+const timer = autoScroll(primaryImages , thumbnailContainers , gameDescriptions);
+
+// Restart auto-scrolling
+btns[1].addEventListener('click' , function () {
+    clearInterval(timer);
+    timer = autoScroll(primaryImages , thumbnailContainers , gameDescriptions);
+});
+
+// Stop auto-scrolling
+btns[2].addEventListener('click' , function () {
+    clearInterval(timer);
+});
+
 // Calling the function on click on arrow, for previous arrow passing false as argument to revert the logic of the function and obtaining the previous item instead of the next one
 btns[0].addEventListener('click' , function () {
     activeTheAdjacent(primaryImages , thumbnailContainers , gameDescriptions , false);
 });
-btns[1].addEventListener('click' , function () {
+btns[3].addEventListener('click' , function () {
     activeTheAdjacent(primaryImages , thumbnailContainers , gameDescriptions);
 });
-
-setInterval(() => activeTheAdjacent(primaryImages , thumbnailContainers , gameDescriptions) , "10000");
 
 
 // Function to check images with active class, remove the class and set as active the one behind or after
@@ -75,4 +88,9 @@ function activeTheAdjacent(largeImages , smallImageContainers , gameDescriptions
     largeImages[index].classList.add('active');
     smallImageContainers[index].classList.add('active');
     gameDescriptionsContainers[index].classList.add('active');
+}
+
+// Auto-scrolling the previews
+function autoScroll(primaryImages , thumbnailContainers , gameDescriptions) {
+    return setInterval(() => activeTheAdjacent(primaryImages , thumbnailContainers , gameDescriptions) , "3000");
 }
